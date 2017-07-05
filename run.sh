851ab7c8d536dfd2cd206d020a6c2b079c43eea0
Alex Vaschillo <lesha@lesha-deb.lesha-home>
2017-07-05 06:24:50 -0700

    added my run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python ./src/process_log.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json
+# the three command line arguments are the input batch file, the input stream file, and the output flagged file
+./src/process_log ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json 2> ./log_output/error_log
